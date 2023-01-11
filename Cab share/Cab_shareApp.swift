a16c1a5ecdd6b90c0ac9b90ae09df29ae89e05fb
slavny11 <slavny45@gmail.com>
2023-01-11 15:18:39 +0100

    Initial Commit

---
+++ Cab share/Cab_shareApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cab_shareApp.swift
+//  Cab share
+//
+//  Created by Viacheslav on 11/01/23.
+//
+
+import SwiftUI
+
+@main
+struct Cab_shareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
