d39d3340cd45f85f380412f3e973249acfd272ab
marcodellisola <marcodellisola@outlook.it>
2023-01-23 13:39:25 +0100

    Changed the url into WeatherManager with URLComponents

--- Cab share/Weather/WeatherManager.swift
+++ Cab share/Weather/WeatherManager.swift
@@ -9,11 +9,19 @@ import Foundation
 import CoreLocation
 
 class WeatherManager {
+    let baseURL = "http://api.openweathermap.org/data/2.5/weather"
+    let appid = "760cb0970ae941895591c62f42d4e50e"
     // MARK: - Request weather to Open Weather
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocationDegrees) async throws -> ResponseBody {
-        guard let url = URL(string:"http://api.openweathermap.org/data/2.5/weather?lat=\(latitude)&lon=\(longitude)&appid=760cb0970ae941895591c62f42d4e50e") else {
-            fatalError("Missing URL")
-        }
+        var urlComponents = URLComponents(string: baseURL)!
+        urlComponents.queryItems = [
+            URLQueryItem(name: "lat", value: "\(latitude)"),
+            URLQueryItem(name: "lon", value: "\(longitude)"),
+            URLQueryItem(name: "appid", value: appid)
+        ]
+
+        let url = urlComponents.url!
+
         let urlRequest = URLRequest(url: url)
         let (data, response) = try await URLSession.shared.data(for: urlRequest)
         guard (response as? HTTPURLResponse)?.statusCode == 200 else {
@@ -23,3 +31,17 @@ class WeatherManager {
         return decodedData
     }
 }
+
+//guard let url: URLComponents = {
+//    URLComponents(string:"http://api.openweathermap.org!)
+//    url.path = "/data/2.5/weather"
+//                  url.queryItems = [
+//                    URLQueryItems(name: "lat", value: latitude)
+//                    URLQueryItems(name: "lon", value: longtitude)
+//                  ]
+//
+//                  lat=\(latitude)&lon=\(longitude)&appid=760cb0970ae941895591c62f42d4e50e")
+//                  else {
+//    fatalError("Missing URL")
+//}
+//}
