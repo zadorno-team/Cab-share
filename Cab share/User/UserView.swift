50c16450ba860df7a68f8de99899c108a98e61d8
alexnikxx <alexnikxx@users.noreply.github.com>
2023-01-24 16:02:25 +0100

    editing user information

--- Cab share/User/UserView.swift
+++ Cab share/User/UserView.swift
@@ -10,6 +10,7 @@ import Firebase
 
 struct UserView: View {
     var nameType: String? = nil
+    @State var isEditing = true
     @EnvironmentObject var userModel: UserModel
     var body: some View {
         NavigationView {
@@ -24,10 +25,6 @@ struct UserView: View {
                             .resizable()
                             .aspectRatio(contentMode: .fit)
                             .clipShape(Circle())
-                        Button("edit") {
-                            
-                        }
-                        .foregroundColor(.white)
                     }
                     .offset(x: 45, y: 0)
                 }
@@ -37,71 +34,57 @@ struct UserView: View {
                     Text("Your name")
                         .font(.headline)
                     HStack {
-                        CustomTextField(placeholder: Text("Print your name"), text: $userModel.nameText)
+                        UserTextField(placeholder: Text("Print your name"), isDisabled: $isEditing, text: $userModel.nameText)
                     }
                     .padding(.bottom, 2)
                     Text("Your age")
                         .font(.headline)
                     HStack {
-                        CustomTextField(placeholder: Text("Print your age"), text: $userModel.ageText)
+                        UserTextField(placeholder: Text("Print your age"), isDisabled: $isEditing, text: $userModel.ageText)
                     }
                     .padding(.bottom, 2)
                     
                     Text("Your hometown")
                         .font(.headline)
                     HStack {
-                        CustomTextField(placeholder: Text("Print your hometown"), text: $userModel.hometownText)
+                        UserTextField(placeholder: Text("Print your hometown"), isDisabled: $isEditing, text: $userModel.hometownText)
                     }
                     .padding(.bottom, 2)
                     
                     Text("Your login")
                         .font(.headline)
                     HStack {
-                        CustomTextField(placeholder: Text("Print your e-mail"), text: $userModel.emailText)
+                        UserTextField(placeholder: Text("Print your e-mail"), isDisabled: $isEditing, text: $userModel.emailText)
                     }
                     .padding(.bottom, 2)
                     
                     Text("Your password")
                         .font(.headline)
                     HStack {
-                        SecureField("Print your password", text: $userModel.passwordText)
+                        UserSecureField(placeholder: Text("Print your password"), isDisabled: $isEditing, text: $userModel.passwordText)
                     }
                     .padding(.bottom, 2)
                 }
                 .padding()
                 Spacer()
-                //                .padding()
-                
             }
-            .onAppear{userModel.download()}
+            //            .onAppear{userModel.download()}
             .navigationTitle("User")
             .preferredColorScheme(.dark)
-            
+            .toolbar {
+                Button("Edit") {
+                    isEditing.toggle()
+                }
+                .foregroundColor(.white)
+            }
         }
-        
     }
-//    func editButtonItem() -> UIBarButtonItem {
-//           let editButton = editButtonItem()
-//           editButton.action = "editButtonAction:"
-//           return editButton
-//      }
-//
-//      func editButtonAction(sender: UIBarButtonItem) {
-//           if self.tableView.editing == true {
-//                self.tableView.editing = false
-//                sender.style = UIBarButtonItemStyle.Plain
-//                sender.title = "Edit"
-//           } else {
-//                self.tableView.editing = true
-//                sender.style = UIBarButtonItemStyle.Done
-//                sender.title = "Done"
-//           }
-//      }
-}
-
-struct UserView_Previews: PreviewProvider {
-    static var previews: some View {
-        UserView()
-            .environmentObject(UserModel())
+    
+    
+    struct UserView_Previews: PreviewProvider {
+        static var previews: some View {
+            UserView()
+                .environmentObject(UserModel())
+        }
     }
 }
